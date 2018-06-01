5606bb6c57789948d86a18b3fb983536a92b1606
Flor Kaucic <florkaucic@gmail.com>
2018-06-01 04:18:23 -0300

    [ardu] agrega libreria para manejar LED con PWM

--- sistema-embebido/sistema-embebido.ino
+++ sistema-embebido/sistema-embebido.ino
@@ -1,9 +1,13 @@
 #include "src/ObstacleDetector/ObstacleDetector.h"
+#include "src/LED/LED.h"
 
 const int TRIGGER_PIN = 50;
 const int ECHO_PIN = 51;
 
+const int LED_PIN = 9;
+
 ObstacleDetector obstacleDetector(TRIGGER_PIN, ECHO_PIN);
+LED led(LED_PIN);
 
 void setup() {
   Serial.begin(9600);
